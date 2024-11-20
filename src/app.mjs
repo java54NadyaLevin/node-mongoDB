b1abcfeee0c93390e773b4b20d90e76314661f26
Nadelele <nadele@gmail.com>
2024-11-20 23:08:01 +0200

    HW69 initial code

--- src/app.mjs
+++ src/app.mjs
@@ -1,15 +1,92 @@
 import MongoConnection from "./mongo/MongoConnection.mjs";
 const DB_NAME = 'sample_mflix';
+const MONGO_URI = 'mongodb+srv://nadele:D62wqRmrFbOX0ljG@cluster0.rffud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const COLLECTION_MOVIES_NAME = "movies";
-const mongoConnection = new MongoConnection(process.env.MONGO_URI, DB_NAME);
+const COLLECTION_COMMENTS_NAME = "comments";
+const mongoConnection = new MongoConnection(MONGO_URI, DB_NAME);
 const collectionMovies = mongoConnection.getCollection(COLLECTION_MOVIES_NAME);
-collectionMovies.aggregate([
-    {
-      '$bucketAuto': {
-        'groupBy': '$imdb.rating', 
-        'buckets': 5
+const collectionComments = mongoConnection.getCollection(COLLECTION_COMMENTS_NAME);
+collectionComments.aggregate([
+  {
+    '$lookup': {
+      'from': 'movies', 
+      'localField': 'movie_id', 
+      'foreignField': '_id', 
+      'as': 'moviesInfo'
+    }
+  }, {
+    '$unwind': '$moviesInfo'
+  }, {
+    '$replaceRoot': {
+      'newRoot': {
+        '$mergeObjects': [
+          '$$ROOT', {
+            'title': '$moviesInfo.title'
+          }
+        ]
       }
     }
-  ]).toArray().then(data=>console.log("first",data));
-collectionMovies.find({}).limit(1).project({'title':1,'_id':0}).toArray()
-.then(data => console.log('second', data))
+  }, {
+    '$project': {
+      'moviesInfo': 0
+    }
+  }, {
+    '$limit': 5
+  }
+] 
+).toArray().then(data=>console.log("========== first ==========",data));
+collectionMovies.aggregate(
+  [
+    {
+      '$facet': {
+        'avgRating': [
+          {
+            '$group': {
+              '_id': null, 
+              'average_imdb_rating': {
+                '$avg': '$imdb.rating'
+              }
+            }
+          }
+        ], 
+        'comedyMovies': [
+          {
+            '$match': {
+              'year': 2010, 
+              'genres': 'Comedy'
+            }
+          }, {
+            '$project': {
+              'title': 1, 
+              'imdb_rating': '$imdb.rating'
+            }
+          }
+        ]
+      }
+    }, {
+      '$unwind': '$comedyMovies'
+    }, {
+      '$project': {
+        'title': '$comedyMovies.title', 
+        'imdb_rating': '$comedyMovies.imdb_rating', 
+        'avgRating': {
+          '$arrayElemAt': [
+            '$avgRating.average_imdb_rating', 0
+          ]
+        }
+      }
+    }, {
+      '$match': {
+        '$expr': {
+          '$gt': [
+            '$imdb_rating', '$avgRating'
+          ]
+        }
+      }
+    }, {
+      '$project': {
+        'title': 1
+      }
+    }
+  ]
+).toArray().then(data=>console.log("========== second ==========",data));;
